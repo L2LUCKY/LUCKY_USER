e90333016e2f5fcf9f1bbfa81f06d9e6c4108ab7
L2LUCKY <145443484+L2LUCKY@users.noreply.github.com>
2024-08-22 06:46:37 +0530

    Update console.py

--- DaxxTeam/console.py
+++ DaxxTeam/console.py
@@ -28,16 +28,16 @@ if os.path.exists("Internal"):
    load_dotenv("Internal")
 
 
-API_ID = int(getenv("API_ID", 0))
-API_HASH = getenv("API_HASH", None)
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-STRING_SESSION = getenv("STRING_SESSION", None)
-MONGO_DB_URL = getenv("MONGO_DB_URL", None)
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", 0))
+API_ID = int(getenv("API_ID", "21767752"))
+API_HASH = getenv("API_HASH", "8817c95b20fca899462336cdf36dd958")
+BOT_TOKEN = getenv("BOT_TOKEN", "7178919717:AAE-B5nEhE8GBLfBHFD0eL1AvDJiVnV3duM")
+STRING_SESSION = getenv("STRING_SESSION", "BQFMJkgADijzuJOv-qLbJS9OaD3BmMvNUqG8zYADGXlvzwRFCl7RmrCBYeckQj3l9W5jCeR71EMQCQku7KjWLl0IEXI_kcI8H71tri81YzdYd-v-uaE5TqEqM-t-y4MKT9THVTkXlupqNmEkewxP_i8jJraykH6ddacfp1oKRwm0aDy-e1Tqbip3VDRsOjkcTjSiO-AbrSV9j_lwgY_-XMIKPO9dUqpIk-BtG2sMLHXNYV2EHZmcSdQxVGpfAFSax8crseCjI0JEDx4c0XkPhDB_wWSuvWDUe_nAi_bdx4GoY0d-Ws5nznclgffyUPzwt9MA9Rm2UUm6hK_4iKWAB37_aF9cxQAAAAE5zUGcAA")
+MONGO_DB_URL = getenv("MONGO_DB_URL", "mongodb+srv://Sarkar123:GAUTAMMISHRA@sarkar.1uiwqkd.mongodb.net/?retryWrites=true&w=majority")
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002242282030"))
 
 
 # OPTIONAL VARIABLES
-SESSION_STRING = getenv("SESSION_STRING", None)
+SESSION_STRING = getenv("SESSION_STRING", "BQFMJkgADijzuJOv-qLbJS9OaD3BmMvNUqG8zYADGXlvzwRFCl7RmrCBYeckQj3l9W5jCeR71EMQCQku7KjWLl0IEXI_kcI8H71tri81YzdYd-v-uaE5TqEqM-t-y4MKT9THVTkXlupqNmEkewxP_i8jJraykH6ddacfp1oKRwm0aDy-e1Tqbip3VDRsOjkcTjSiO-AbrSV9j_lwgY_-XMIKPO9dUqpIk-BtG2sMLHXNYV2EHZmcSdQxVGpfAFSax8crseCjI0JEDx4c0XkPhDB_wWSuvWDUe_nAi_bdx4GoY0d-Ws5nznclgffyUPzwt9MA9Rm2UUm6hK_4iKWAB37_aF9cxQAAAAE5zUGcAA")
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", ". !").split())
 
 
